51429ecd5eb786dd0095eae180836dc832477cc4
Iryna Zaitseva <zaitseva02@gmail.com>
2023-08-07 17:24:11 +0300

    project dom

---
+++ src/pages/Main.js
@@ -0,0 +1,13 @@
+// import Header from "../components/Header";
+// import Characters from "./Characters";
+
+const Main = () => {
+  return (
+    <div>
+      {/* <Header />
+      <Characters /> */}
+    </div>
+  );
+};
+
+export default Main;
