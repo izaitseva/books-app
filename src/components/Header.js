713f83b31f9d311d78b89ecf17669df60b338de9
Iryna Zaitseva <zaitseva02@gmail.com>
2023-08-08 18:15:44 +0300

    header styles done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,14 @@
 import { NavLink } from "react-router-dom";
 import { paths } from "../paths";
+import styles from "./Header.module.css"
 import logo from "../images/header-logo.svg";
 
 const Header = () => {
   return (
     <nav>
-      <header>
+      <header className={styles.header_styles}>
         <NavLink to={paths.main}>
-          <img src={logo} alt="logo" />
+          <img src={logo} alt="logo" width="28"/>
         </NavLink>
       </header>
     </nav>
