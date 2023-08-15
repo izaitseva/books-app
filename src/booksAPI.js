961113a8961a5ea4834516c4f412945ec4d8dedb
Iryna Zaitseva <zaitseva02@gmail.com>
2023-08-15 19:23:52 +0300

    some updates

---
+++ src/booksAPI.js
@@ -0,0 +1,15 @@
+// import axios from "axios";
+
+// const URL = "https://bookread-backend.goit.global/api-docs/books";
+
+// export const fetchBooksById = (bookId) => {
+//   return axios.get(`${URL}/${bookId}`);
+// };
+
+// export const fetchBooks = (name) => {
+//   return axios.get(`${URL}/?name=${name ?? ""}`);
+// };
+
+// export const fetchFilterBooks = (characterFilter) => {
+//   return axios.get(`${URL}?name=${characterFilter}`);
+// };
--- src/pages/Library.js
+++ src/pages/Library.js
@@ -1,12 +1,15 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
 import styles from "./Library.module.css";
 import point from "../images/point-icon.svg";
 import flag from "../images/flag-icon.svg";
-import book from "../images/book-icon.svg";
-// import { useState } from "react";
+import bookicon from "../images/book-icon.svg";
 
 export const Library = () => {
-  // const [bookTitle, setBookTitle] = useState("");
-  // const [books, setBooks] = useState([]);
+  const [bookTitle, setBookTitle] = useState("");
+
+  useEffect(() => {});
 
   return (
     <div className={styles.main_container}>
@@ -14,8 +17,8 @@ export const Library = () => {
         <div className={styles.input_div}>
           <label className={styles.label}>Book title</label>
           <input
-            // value={bookTitle}
-            // onChange={(e) => setBookTitle(e.target.value)}
+            value={bookTitle}
+            onChange={(e) => setBookTitle(e.target.value)}
             className={`${styles.input} ${styles.title_input} `}
             type="textarea"
             id="textarea"
@@ -26,8 +29,8 @@ export const Library = () => {
           <label className={styles.label}>Author</label>
           <input
             className={`${styles.author_input} ${styles.input}`}
-            type="password"
-            id="password"
+            type="search"
+            id="search"
             placeholder="..."
           />
         </div>
@@ -49,7 +52,7 @@ export const Library = () => {
           <li className={styles.list_item}>
             <h2 className={styles.list_title}>Step 1.</h2>
             <div className={styles.header_list}>
-              <img className={styles.icon} src={book} alt="point" />
+              <img className={styles.icon} src={bookicon} alt="point" />
               <h3 className={styles.list_header}>Create your own library</h3>
             </div>
             <div className={styles.list_div}>
@@ -73,6 +76,44 @@ export const Library = () => {
             </div>
           </li>
         </ul>
+      </div>
+      {/* Already read */}
+      <div>
+        <h1>Already read</h1>
+        <table>
+          <tr>
+            <th>Book title</th>
+            <th>Author</th>
+            <th>Year</th>
+            <th>Pages</th>
+            <th>Rating</th>
+          </tr>
+          <tr>
+            <td>Book title </td>
+            <td>19</td>
+            <td>Male</td>
+            <td>Male</td>
+            <td>5 stars</td>
+          </tr>
+        </table>
+      </div>
+      {/* reading now */}
+      <div>
+        <h1>Reading now</h1>
+        <table>
+          <tr>
+            <th>Book title</th>
+            <th>Author</th>
+            <th>Year</th>
+            <th>Pages</th>
+          </tr>
+          <tr>
+            <td>Book title </td>
+            <td>19</td>
+            <td>Male</td>
+            <td>Male</td>
+          </tr>
+        </table>
       </div>
       {/* going to read */}
       <div>
